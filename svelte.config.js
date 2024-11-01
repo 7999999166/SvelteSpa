ba0e86d2a9627b04381e62c31cf88441d34fb31f
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2024-11-02 02:28:45 +0530

    cccnv

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(
 			{
-              fallback: '200.html' 
+              fallback: 'index.html' 
             }
 		)
 	}
